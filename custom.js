c48bf67128a9512c5210a22f0ef9039c11dc984a
yaw595 <donkoryw94@gmail.com>
2018-06-30 03:25:28 +0000

    one more

--- custom.js
+++ custom.js
@@ -1,24 +1,17 @@
-$(document).ready(function () {
-    $(".check.mdi").click(function () {
-      $(this).toggleClass("mdi-check-circle mdi-check-circle-outline");
-    });
-  });
-  
-  $(document).ready(function () {
-    $("#btn1").click(function () {
-      $('.card-img-top').hide(1000);
-      $('.avatar').hide(1000);
-      $('#cbd1').hide(1000);
-      $('#cb2').show(1000); 
-    });
-    
-    $("#btn2").click(function() {
-      $('#cb2').hide(1000);
-      $('.card-img-top').show(1000);
-      $('.avatar').show(1000);
-      $('#cbd1').show(1000);
-    });
-    
-  });
-  
-  
+$(".check.mdi").click(function () {
+    $(this).toggleClass("mdi-check-circle mdi-check-circle-outline");
+}); 
+
+$("#btn1").click(function () {
+    $('.card-img-top').hide(1000);
+    $('.avatar').hide(1000);
+    $('#cbd1').hide(1000);
+    $('#cb2').show(1000);
+});
+
+$("#btn2").click(function () {
+    $('#cb2').hide(1000);
+    $('.card-img-top').show(1000);
+    $('.avatar').show(1000);
+    $('#cbd1').show(1000);
+});
